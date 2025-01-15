113d12ce4762ce9363a8fa32181f11bfda133733
Pierrick Coulon <pierrick.coulon@logsolution.fr>
2025-01-15 16:33:53 +0100

    fix: :art: improve sepa logic

--- flutter_app/lib/services/sepa_service.dart
+++ flutter_app/lib/services/sepa_service.dart
@@ -6,10 +6,10 @@ class SepaService {
       final root = document.rootElement;
       if (root.name.local != 'Document') return false;
 
-      final transfer = root.findElements('CstmrCdtTrfInitn');
-      if (transfer.isEmpty) return false;
+      final pain = root.findElements('pain.001.001.02');
+      if (pain.isEmpty) return false;
 
-      final pmtInf = transfer.first.findElements('PmtInf');
+      final pmtInf = pain.first.findElements('PmtInf');
       if (pmtInf.isEmpty) return false;
 
       return true;
@@ -43,39 +43,19 @@ class SepaService {
     final pmtInfElements = document.findAllElements('PmtInf');
 
     for (var pmtInf in pmtInfElements) {
-      var pmtTpInf = pmtInf.findElements('PmtTpInf').firstOrNull;
-      if (pmtTpInf == null) {
-        pmtTpInf = XmlElement(XmlName('PmtTpInf'));
-        final reqdExctnDt = pmtInf.findElements('ReqdExctnDt').firstOrNull;
-        if (reqdExctnDt != null) {
-          final index = pmtInf.children.indexOf(reqdExctnDt);
-          pmtInf.children.insert(index + 1, pmtTpInf);
-        } else {
-          pmtInf.children.insert(0, pmtTpInf);
+      final pmtTpInf = pmtInf.findElements('PmtTpInf').firstOrNull;
+      if (pmtTpInf != null) {
+        final svcLvl = pmtTpInf.findElements('SvcLvl').firstOrNull;
+        if (svcLvl != null) {
+          final lclInstrm = XmlElement(XmlName('LclInstrm'));
+          final lclInstrmCd = XmlElement(XmlName('Cd'));
+          lclInstrmCd.children.add(XmlText('INST'));
+          lclInstrm.children.add(lclInstrmCd);
+
+          final svcLvlIndex = pmtTpInf.children.indexOf(svcLvl);
+          pmtTpInf.children.insert(svcLvlIndex + 1, lclInstrm);
         }
       }
-
-      var svcLvl = pmtTpInf.findElements('SvcLvl').firstOrNull;
-      if (svcLvl == null) {
-        svcLvl = XmlElement(XmlName('SvcLvl'));
-        pmtTpInf.children.add(svcLvl);
-      } else {
-        svcLvl.children.clear();
-      }
-      final svcLvlCd = XmlElement(XmlName('Cd'));
-      svcLvlCd.children.add(XmlText('SEPA'));
-      svcLvl.children.add(svcLvlCd);
-
-      var lclInstrm = pmtTpInf.findElements('LclInstrm').firstOrNull;
-      if (lclInstrm == null) {
-        lclInstrm = XmlElement(XmlName('LclInstrm'));
-        pmtTpInf.children.add(lclInstrm);
-      } else {
-        lclInstrm.children.clear();
-      }
-      final lclInstrmCd = XmlElement(XmlName('Cd'));
-      lclInstrmCd.children.add(XmlText('INST'));
-      lclInstrm.children.add(lclInstrmCd);
     }
   }
 }
